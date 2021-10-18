42765535df0cca7eb576a2d72c573a70fa9e390e
sigi <sigineumann@gmx.de>
2021-10-18 19:41:15 +0200

    lesson 2

---
+++ Kursbuch/wichtige_links.md
@@ -0,0 +1,14 @@
+Wichtige Links
+github.com
+
+https://code.visualstudio.com/docs/languages/python
+
+https://github.com/SigiNeumann/VHSKurs
+
+https://www.tiobe.com/tiobe-index/
+
+https://insights.stackoverflow.com/survey/2020#technology-most-loved-dreaded-and-wanted-languages-wanted
+
+https://py-tutorial-de.readthedocs.io/de/python-3.3/appetite.html#
+
+https://www.python-forum.de/
---
+++ uebungen/if_else.py
@@ -0,0 +1,51 @@
+# Wenn a grosser b => gibt "ja" zurück, wenn a und b gleich sing gib "sind gleich" zurück, ansonsten "b ist grösser"
+def a_grosser_b(a:int, b:int)->str:
+    pass
+
+#Gegeben werden drei Zahlen gibt die größte zurück
+def die_groesste_zahl(a:int,b:int,c:int)->bool:
+    pass
+
+#Schreibe eine Funktion die 3 Werte entgegennimmt, wenn alle drei gleich sind gibt den Wert mal 3 zurück 
+#Ansonsten berechne die Summe der 3 Zahlen
+def summe_der_drei(a:int,b:int,c:int)->int:
+    pass
+
+#Überprüfe ob einen gegebene Zahl ein
+def ist_gerade_zahl(a:int)->bool:
+    pass
+
+#Schreibe eine Funktion die einen Name entgegennimmt ist der Name Bond, schreibe Willkommen an Bord 007.
+#Ansonsten schreibe Hallo und den Name
+def hallo(name:str):
+    pass
+
+#Wenn der Bruttpreis größer als 1000€ ist bekommt der Kunde einen Rabatt von 10%
+#Wenn er bar bezahlt bekommt er 3% Rabatt jedoch nicht wenn er schon den anderen Rabat bekommen hat
+#Gib den zu bezahlenden Preis zurück
+def bezahlen(bruttopreis:float,zahlt_bar:bool):
+    pass
+
+
+#Der Notenschlüssel für eine Prüfunung ist wie folgt:
+#Prozent >= 90% : Grade 1
+#Prozent >= 80% : Grade 2
+#Prozent >= 70% : Grade 3
+#Prozent >= 60% : Grade 4
+#Prozent >= 50% : Grade 5
+#Prozent < 50% : Grade 6
+#Gib die passende Note zurück
+def note(prozent:int)->int:
+    pass
+
+
+#Gibt zurück ob ein Jahr z.b. 2000 ein Schaltjahr ist folgende regeln gelten hierfür
+#1. Schaltjahre sind durch 4 teilbar
+#2. Ist das Jahr durch 100 teilbar, dann ist es kein Schaltjahr
+#3. Ist das Jahr durch 400 teilbar, dann ist es trotzdem ein Schaltjahr 
+def ist_ein_schaltjahr(a:int):
+    pass    
+
+#geben wird ein zahl, finde die nächst größere gerade zahl und gib sie zurück
+def die_naechst_groessere_gerade_zahl(a:int):
+    pass
